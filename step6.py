f7c1cd4836b3c539a1669a649ac3e868cc75d1de
MeriemLil <ine_meriem@hotmail.com>
2020-10-18 08:38:34 -0700

    Step 6 completed

---
+++ step6.py
@@ -0,0 +1,33 @@
+import nltk
+from nltk import word_tokenize
+from nltk.stem import PorterStemmer
+from nltk.corpus import wordnet as wn, stopwords
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+#tokenize the original data
+file_content = open("./data/all_data.txt").read()
+tokens = nltk.word_tokenize(file_content)
+
+#suggest various filtering strategies
+def preprocess(sentence):
+    stop_words = set(stopwords.words("English"))
+    #tokenization and stopwords removal
+    words = word_tokenize(sentence)
+    filtered_words = [word for word in words if word.isalpha() and word not in stop_words]
+    #stemming
+    stemmed = []
+    for w in filtered_words:
+        stemmed.append(PorterStemmer().stem(w))
+    #position tag
+    tagged1 = nltk.pos_tag(words)
+    tagged2 = nltk.pos_tag(filtered_words)
+    return words, filtered_words, stemmed, tagged1, tagged2 #depending on the use select from the return list
+
+#split the original data into 70% training and 30% testing
+dataset = pd.read_csv('./data/all_data.txt', header=None, names=['text','label'], sep=';')
+dataset.to_csv("./data/all_data.csv")
+x = dataset['text']
+y = dataset['label']
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3)
+
