e045e40bc91d5c62c8248bba006133093e0b6a03
MeriemLil <ine_meriem@hotmail.com>
2020-10-18 17:42:07 -0700

    Step 5 completed, database analyzed using the SentiStrength tool, modified accordingly

--- step5.py
+++ step5.py
@@ -1,26 +1,23 @@
 import pandas as pd
 # the SentiStrength tool calculates the positive and negative scores of a sentence
-# we feed to it the combined database train-test-val as txt file and it returns the results as txt file
-# for readability we read the results as excel file
-sentiStrength = pd.read_excel('sentistrength\sentistrength_results_excel.xlsx')
+# we feed to it the combined database train-test-val as txt file 'concatenated_data.txt' and it returns the results as txt file 'concatenated_data+results.txt'
 
-# then we convert to csv for manipulation
-sentiStrength.to_csv("sentistrength\sentistrength_results_csv.csv", header=True)
-sentiStrength_results = pd.read_csv('sentistrength\sentistrength_results_csv.csv')
+sentiStrength = pd.read_csv('sentistrength\concatenated_data+results.txt', sep= '\t')
 
 # We add a column for summing the negative and positive scores
-sentiStrength_results['Sum'] = ''
-for i in range(len(sentiStrength_results)):
-    sentiStrength_results['Sum'][i] = sentiStrength_results['Positive'][i] + sentiStrength_results['Negative'][i]
+sentiStrength['Sum'] = ''
+for i in range(len(sentiStrength)):
+    sentiStrength['Sum'][i] = sentiStrength['Positive'][i] + sentiStrength['Negative'][i]
 
-# we save in the same file
-sentiStrength_results.to_csv('sentistrength\sentistrength_results_csv.csv')
+# we save the results
+sentiStrength.to_csv('sentistrength\sentistrength_results.csv')
 
-# The sentiment score can be used an indicator to distinguish between the positive sentiments (love, joy, surprise) and the negative sentiments (anger, sadness, fear)
-# For positive sentiments we get an overall score that is positive, and for the negative ones the overall score is negative
-# Thus, discriminating the various emotion states is not possible, as this classification only tells us about the positive and negative sentiments and it does not separate
-# between states that belong to positive or those who belong to negative.
-
+# The sentiment score can be used as a rough indicator to distinguish between the positive sentiments (love, joy, surprise) and the negative sentiments (anger, sadness, fear)
+# In general, for positive sentiments we get an overall score that is positive, and for the negative ones the overall score is negative
+# sometimes an ambiguity rises when score is 0, indicating neither positive nor negative but neutral
+# Thus, discriminating the various emotion states is not possible, because this classification only tells us about barely the positive and negative sentiments and it does not
+# separate between states that belong to positive or those who belong to negative.
+# Also because sometimes a positive or a negative sentiment is reported as neutral
 
 
 
