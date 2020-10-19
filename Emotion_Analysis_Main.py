9f61e1801ae795c4c504e2efa2728acfcdccd6ca
Lauri Heikka <lauri.heikka@gmail.com>
2020-10-19 13:13:14 +0300

    fixed main conflict

--- Emotion_Analysis_Main.py
+++ Emotion_Analysis_Main.py
@@ -1,27 +1,24 @@
-import nltk
-from nltk import word_tokenize
-from nltk.corpus import wordnet as wn, stopwords
 import pandas as pd
-import re
-from empath import Empath
-
-#These are our six categories
-anger = wn.synset("anger.n.01")
-love = wn.synset("love.n.01")
-sadness = wn.synset("sadness.n.01")
-surprise = wn.synset("surprise.n.01")
-joy = wn.synset("joy.n.01")
-fear = wn.synset("fear.n.01")
-#joined in a list
-categories = [anger, love, sadness, surprise, joy, fear]
-
-# import data files
-train = pd.read_csv('./data/train.txt', header=None, names=['text','label'], sep=';')
-test = pd.read_csv('./data/test.txt', header=None, names=['text','label'], sep=';')
-val = pd.read_csv('./data/val.txt', header=None, names=['text','label'], sep=';')
+from patternMatch.categorySetup import * 
+from patternMatch.constants import *
+from patternMatch.stringMatching import *
+from sqlalchemy import create_engine
+engine = create_engine('sqlite:///./data/project.db', echo=False)
 
 #Here we open the Harvard inquirer XL file
 harvardInquirer = pd.read_excel('./data/inquirerbasic.xls')
 
+# task1
+if not engine.has_table('harvardWords'):
+    is_database_created = assign_category(category_list, harvardInquirer)
+    print('Database created successfully')
+else:
+    print("Database creation Done")
 
 
+# task2
+if engine.has_table('harvardWords'):
+    result = engine.execute('SELECT * FROM harvardWords')
+    string_match(result)
+else:
+    print('Database is not exist')
