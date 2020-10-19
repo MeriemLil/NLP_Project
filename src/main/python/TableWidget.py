d0cd21cb3a1d32c5cbc43e99bedfc456fe322017
Lauri Heikka <lauri.heikka@gmail.com>
2020-10-19 22:38:46 +0300

    fixes on tablewidget

--- src/main/python/TableWidget.py
+++ src/main/python/TableWidget.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from PyQt5.QtWidgets import QWidget,QScrollArea, QTableWidget, QVBoxLayout,QTableWidgetItem
+from PyQt5.QtWidgets import *
 from PyQt5 import QtCore
 from sqlalchemy import create_engine
 
@@ -13,16 +13,34 @@ class TableWidget(QWidget):
         self.initUI()
 
     def initUI(self):
+        grid = QGridLayout()
+        self.setLayout(grid)
+        
+        dropdown = QComboBox()
+        dropdown.addItems(self.df.columns[:-1])
+        grid.addWidget(dropdown, 0, 0)
+        
+        btn1 = QPushButton('Show table', self)
+        btn1.resize(btn1.sizeHint())
+        btn1.clicked.connect(lambda: self.populate(col=dropdown.currentText(), table=table))
+        grid.addWidget(btn1, 0, 1)
+    
         scroll = QScrollArea()
         layout = QVBoxLayout()
         table = QTableWidget()
         scroll.setWidget(table)
-        layout.addWidget(table)
-        self.setLayout(layout)    
-
-        table.setColumnCount(len(self.df.columns))
-        table.setRowCount(len(self.df.index))
-        for i in range(len(self.df.index)):
-            for j in range(len(self.df.columns)):
-                table.setItem(i,j,QTableWidgetItem(str(self.df.iloc[i, j])))
+        grid.addWidget(table) 
         self.show()
+        
+     
+    def populate(self, col, table):
+        dataframe = self.df.groupby(col)['score'].agg('mean') * 100
+        dataframe = dataframe.round(2)
+        table.setColumnCount(2)
+        table.setRowCount(len(dataframe.index)+1)
+        table.setItem(0,0,QTableWidgetItem(col))
+        table.setItem(0,1,QTableWidgetItem('score'))
+        for i in range(1, len(dataframe.index)+1):
+            table.setItem(i,0,QTableWidgetItem(str(dataframe.index[i-1])))
+            table.setItem(i,1,QTableWidgetItem(str(dataframe.iloc[i-1])+'%'))
+        
