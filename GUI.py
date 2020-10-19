c4053227f60ccc63a5b3c884d87fb908f3e09c81
MeriemLil <ine_meriem@hotmail.com>
2020-10-19 13:02:54 -0700

    import GUI in main then call GUI.gui()

--- GUI.py
+++ GUI.py
@@ -5,31 +5,32 @@ def on_button_clicked(val):
     alert.setText(val)
     alert.exec_()
 
-app = QApplication([])
-app.setStyle('Fusion')
+def gui():
+    app = QApplication([])
+    app.setStyle('Fusion')
 
-window = QWidget()
-window.setWindowTitle('Emotion Analysis')
-window.setGeometry(50, 50, 400, 100)
+    window = QWidget()
+    window.setWindowTitle('Emotion Analysis')
+    window.setGeometry(50, 50, 400, 100)
 
-layout = QVBoxLayout()
-label = QLabel('Find Accuracy')
-layout.addWidget(label)
+    layout = QVBoxLayout()
+    label = QLabel('Find Accuracy')
+    layout.addWidget(label)
 
-button1 = QPushButton('String Matching')
-button2 = QPushButton('Empath Client')
-button3 = QPushButton('Semantic Similarity')
+    button1 = QPushButton('String Matching')
+    button2 = QPushButton('Empath Client')
+    button3 = QPushButton('Semantic Similarity')
 
-button1.clicked.connect(lambda: on_button_clicked('0.5'))
-button2.clicked.connect(lambda: on_button_clicked('0.3'))
-button3.clicked.connect(lambda: on_button_clicked('0.2'))
+    button1.clicked.connect(lambda: on_button_clicked('0.5'))
+    button2.clicked.connect(lambda: on_button_clicked('0.3'))
+    button3.clicked.connect(lambda: on_button_clicked('0.2'))
 
-layout.addWidget(button1)
-layout.addWidget(button2)
-layout.addWidget(button3)
+    layout.addWidget(button1)
+    layout.addWidget(button2)
+    layout.addWidget(button3)
 
-window.setLayout(layout)
+    window.setLayout(layout)
 
-window.show()
+    window.show()
 
-app.exec_()
+    app.exec_()
