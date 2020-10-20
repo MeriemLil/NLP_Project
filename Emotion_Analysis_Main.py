1135893dc46d2530f9bdd4592348930434a2e097
MeriemLil <ine_meriem@hotmail.com>
2020-10-20 14:38:12 -0700

    steps 1-5 on main code

--- Emotion_Analysis_Main.py
+++ Emotion_Analysis_Main.py
@@ -7,6 +7,7 @@ from patternMatch.empathClient import *
 from sqlalchemy import create_engine
 engine = create_engine('sqlite:///./data/project.db', echo=False)
 
+
 #Here we open the Harvard inquirer XL file
 harvardInquirer = pd.read_excel('./data/inquirerbasic.xls')
 
@@ -36,4 +37,27 @@ print('Accuracy of Empath: ', empath_accuracy)
 # calculate accuracy of empath using exact categories
 accuracy_exact_category = empath_exact_category_accuracy(data, categories)
 print('Accuracy of Empath using exact categories: ', accuracy_exact_category)
+
+# task 4
+csv_one = pd.read_csv( "data/combined_cvs/combined_one.csv")
+csv_all = pd.read_csv( "data/combined_cvs/combined_all.csv")
+
+#variables to count the matching of the original label and the predicted label of a sentence
+count_one = 0
+count_all = 0
+# This count is needed in the calculation of accuracy
+for i in range(len(csv_all)):
+    if csv_one['Original Label'][i] == csv_one['Predicted Label'][i]:
+        count_one += 1
+    if csv_all['Original Label'][i] == csv_all['Predicted Label'][i]:
+        count_all += 1
+
+accuracy_combined_one = count_one/len(csv_one)
+accuracy_combined_all = count_all/len(csv_all)
+#
+print("Accuracy of semantic similarity, 1 synset of a category approach: ", accuracy_combined_one)
+print("Accuracy of semantic similarity, all synsets of a category approach: ", accuracy_combined_all)
+
+# task 5
+senti = pd.read_csv('data\sentistrength\sentistrength_results.csv')
 
